8fae11fe859abd7cf4b99169bd4967e4c872fe75
IsaRK <isabelle.riverain@gmail.com>
2021-03-23 17:00:08 +0100

    fix script

--- runApp.ps1
+++ runApp.ps1
@@ -1,4 +1,4 @@
-$p = Start-Process -FilePath ".\BernardeSaturdayCakes\bin\Release\netcoreapp3.1\BernardeSaturdayCakes.exe" -Wait -PassThru
+$p = Start-Process -FilePath "BernardeSaturdayCakes.exe" -WorkingDirectory "C:\Users\Isabelle\source\repos\BernardeSaturdayCakes\BernardeSaturdayCakes\bin\Release\netcoreapp3.1"  -Wait -PassThru
 
 if($p.ExitCode -ne 0)
 {
